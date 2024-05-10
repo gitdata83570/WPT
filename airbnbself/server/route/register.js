495c1cc79016bb8b9527e70794903eff07f18603
KD4-Pratik-83570 <pratekkmore@gmail.com>
2024-05-10 15:12:30 +0530

    Assignment7 Uploaded

---
+++ airbnbself/server/route/register.js
@@ -0,0 +1,64 @@
+
+
+
+
+const express=require('express');
+const app=express.Router();
+const mysql=require('mysql2');
+
+
+
+
+const connectionString = {
+    host: "localhost",
+    port: 3306,
+    database: "airbnb_db",
+    user: "root",
+    password: "manager"
+ };
+
+ app.post("/",(request,response)=>{
+    var firstName = request.body.firstName;
+    var lastName = request.body.lastName;
+    var email = request.body.email;
+    var password=request.body.password;
+    var isDeleted=request.body.isDeleted;
+    var phoneNumber=request.body.phoneNumber;
+
+    var connection = mysql.createConnection(connectionString);
+    connection.connect();
+
+    let queryText = 
+        `insert into user(firstName,lastName,email,password,phoneNumber,isDeleted) values('${firstName}','${lastName}','${email}','${password}','${phoneNumber}',${isDeleted})`;
+
+    console.log(queryText);
+    connection.query(queryText, (err, result)=>{
+                                                    response.setHeader("Content-Type", "application/json");
+                                                    if(err==null)
+                                                    {
+                                                        response.write(JSON.stringify(result));
+                                                        connection.end();
+                                                        response.end();
+                                                    }
+                                                    else
+                                                    {
+                                                        response.write(JSON.stringify(err));
+                                                        connection.end();
+                                                        response.end();
+                                                    }
+                                                });
+})
+/*
+POST  http://127.0.0.1:9999/register
+
+{
+    "firstName": "pratik",
+    "lastName": "more",
+    "email": "pratekkmore@gmail.com",
+    "password": "pratik@123",
+    "isDeleted": 0,
+     "phoneNumber": "937089082"
+}
+*/
+
+module.exports=app;
